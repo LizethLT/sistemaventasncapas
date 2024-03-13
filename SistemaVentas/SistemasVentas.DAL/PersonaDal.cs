a0cc2a87a4081c871d6776754120bce012f5b064
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2024-03-13 10:09:02 -0400

    nuevo script de la base de  datos

---
+++ SistemaVentas/SistemasVentas.DAL/PersonaDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class PersonaDal
+    {
+        public DataTable ListarPerosnasDal() 
+        {
+            string consulta = "Select * from Persona";
+            DataTable lista = conexion.EjecutarDataTabla(consulta, "Tabla");
+            return lista;   
+
+        }
+    }
+}
